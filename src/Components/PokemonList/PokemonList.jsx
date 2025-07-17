eb5ec93c14f5c62633d1ac1d6040efc0c9f51fad
Prathamesh Santosh Deshmukh <prathameshsantoshdeshmukh123@gamil.com>
2025-07-18 03:09:52 +0530

    Added css to pokedex

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -1,46 +1,57 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import './Pokemon.css';
+import "./PokemonList.css";
 import Pokemon from "../Pokemon/Pokemon";
 
-function PokemonList(){
-    const [pokemonList,setPokemonList] = useState([]);
-    const [isloading,setIsLoading] = useState(true);
+function PokemonList() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
 
-    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon';
+  const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
 
-    async function downloadPokemons() {
-        const response = await axios.get(POKEDEX_URL);
-        const pokemonResults = response.data.results;
-        const pokemonResultPromise = pokemonResults.map((pokemon)=>axios.get(pokemon.url));
-        const pokemonData = await axios.all(pokemonResultPromise);
-        console.log(pokemonData);
-        const pokeListResult =pokemonData.map((pokeData)=>{
-            const pokemon = pokeData.data;
-            return {
-                id : pokemon.id,
-                name : pokemon.name,
-                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,  
-                types: pokemon.types};
-        });
-        console.log(pokeListResult);
-        setPokemonList(pokeListResult);
-        setIsLoading(false);
-    }
+  async function downloadPokemons() {
+    const response = await axios.get(POKEDEX_URL);
+    const pokemonResults = response.data.results;
+    const pokemonResultPromise = pokemonResults.map((pokemon) =>
+      axios.get(pokemon.url)
+    );
+    const pokemonData = await axios.all(pokemonResultPromise);
+    console.log(pokemonData);
+    const pokeListResult = pokemonData.map((pokeData) => {
+      const pokemon = pokeData.data;
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other
+          ? pokemon.sprites.other.dream_world.front_default
+          : pokemon.sprites.front_shiny,
+        types: pokemon.types,
+      };
+    });
+    console.log(pokeListResult);
+    setPokemonList(pokeListResult);
+    setIsLoading(false);
+  }
 
-    useEffect(()=>{
-        downloadPokemons();
-    },[]);
+  useEffect(() => {
+    downloadPokemons();
+  }, []);
 
-    return (
-        <div className="pokemon-list-wrapper">
-            <h2>Pokemon List</h2>
-            {(isloading)? "Loading...." : 
-              pokemonList.map((p)=><Pokemon name={p.name} image={p.image} key={p.id}/>)
-            }
-
-        </div>
-    )
+  return (
+    <div className="pokemon-list-wrapper">
+      <div className="pokemon-wrapper">
+        {isloading
+          ? "Loading...."
+          : pokemonList.map((p) => (
+              <Pokemon name={p.name} image={p.image} key={p.id} />
+            ))}
+      </div>
+      <div className="controls">
+        <button>Prev</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 }
 
 export default PokemonList;
