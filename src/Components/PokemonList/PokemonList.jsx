0b839c170bf976f7b8fc587ef9af43da2ea46bc3
Prathamesh Santosh Deshmukh <prathameshsantoshdeshmukh123@gamil.com>
2025-07-18 04:27:42 +0530

    Added pagination

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -7,11 +7,19 @@ function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
   const [isloading, setIsLoading] = useState(true);
 
-  const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
+  const [pokedexUrl, setPokedexUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
 
   async function downloadPokemons() {
-    const response = await axios.get(POKEDEX_URL);
+    setIsLoading(true);
+    const response = await axios.get(pokedexUrl);
     const pokemonResults = response.data.results;
+    console.log(response.data);
+    setNextUrl(response.data.next);
+    setPrevUrl(response.data.previous);
     const pokemonResultPromise = pokemonResults.map((pokemon) =>
       axios.get(pokemon.url)
     );
@@ -35,7 +43,7 @@ function PokemonList() {
 
   useEffect(() => {
     downloadPokemons();
-  }, []);
+  }, [pokedexUrl]);
 
   return (
     <div className="pokemon-list-wrapper">
@@ -47,8 +55,18 @@ function PokemonList() {
             ))}
       </div>
       <div className="controls">
-        <button>Prev</button>
-        <button>Next</button>
+        <button
+          disabled={prevUrl === null}
+          onClick={() => setPokedexUrl(prevUrl)}
+        >
+          Prev
+        </button>
+        <button
+          disabled={nextUrl === null}
+          onClick={() => setPokedexUrl(nextUrl)}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
